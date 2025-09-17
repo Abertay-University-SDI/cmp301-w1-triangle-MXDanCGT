8818db7b11b37cfe74b350889262b21ed2d7aa31
MXDanCGT <2300399@uad.ac.uk>
2025-09-17 17:32:31 +0100

    Lab 1 work (before messing with UVs)

--- E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
@@ -5,10 +5,14 @@ struct InputType
 {
 	float4 position : SV_POSITION;
 	float4 colour : COLOR;
-};
+}; 
 
 
 float4 main(InputType input) : SV_TARGET
 {
-	return input.colour;
+	//return input.colour;
+	//Temp change - make it red tinge
+    return input.colour * float4(1.f, 0.5f, 0.5f, 0.5f);
+	//Shouldnt we be able to see this stepping through the shaders?
+
 }
--- E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
@@ -25,16 +25,28 @@ OutputType main(InputType input)
 {
 	OutputType output;
 	
+
+	
 	// Change the position vector to be 4 units for proper matrix calculations.
-	input.position.w = 1.0f;
-    
+	input.position.w = 1.f;
+
+	//Double in size		
+    const float4x4 DoubleScale = float4x4(2.f, 0.f, 0.f, 0.f,
+										  0.f, 2.f, 0.f, 0.f,
+										  0.f, 0.f, 2.f, 0.f,
+										  0.f, 0.f, 0.f, 1.f);
+	
+	//IN THEORY this should work??
+    output.position = mul(input.position, DoubleScale);
+
 	// Calculate the position of the vertex against the world, view, and projection matrices.
-	output.position = mul(input.position, worldMatrix);
+	output.position = mul(output.position, worldMatrix);
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
 	output.colour = input.colour;
 	
+
 
 	return output;
 }
